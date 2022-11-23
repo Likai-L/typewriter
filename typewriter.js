3a317aed2d87e81f159ac06b8d9c4a1594584dc1
Likai Liu <likailiu2@gmail.com>
2022-11-23 03:12:00 +0000

    Implement a function that outputs animated sentences

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (let char of sentence) {
+  setTimeout(() => {process.stdout.write(char)}, delay);
+  delay += 100;
+}
+setTimeout(() => {process.stdout.write("\n")}, delay);
